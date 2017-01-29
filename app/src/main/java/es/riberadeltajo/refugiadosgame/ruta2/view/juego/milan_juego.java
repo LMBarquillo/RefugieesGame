bf00fcda16f97e6b1a338d28120aa0d8965312b7
RobertoRamosRodriguez <lagarterano84@hotmail.com>
2017-01-29 11:05:38 +0100

    Primer Envío

--- app/src/main/java/es/riberadeltajo/refugiadosgame/ruta2/view/juego/milan_juego.java
+++ app/src/main/java/es/riberadeltajo/refugiadosgame/ruta2/view/juego/milan_juego.java
@@ -1,22 +1,15 @@
-package es.riberadeltajo.refugiadosgame.ruta3.view;
+package es.riberadeltajo.refugiadosgame.ruta2.view.juego;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import es.riberadeltajo.refugiadosgame.R;
 
-public class Sarajevo extends AppCompatActivity {
-
+public class milan_juego extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sarajevo);
-
-
-
-
+        setContentView(R.layout.activity_milan_juego);
     }
-
-
 }
