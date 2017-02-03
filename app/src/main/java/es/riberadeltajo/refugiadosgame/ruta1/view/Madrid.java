0d9c457293b1179cfc6e2f565d03ca0f2d05ff47
josecarlossanchez <elmejor10tala@gmail.com>
2017-02-03 01:57:34 +0100

    prueba jose

--- app/src/main/java/es/riberadeltajo/refugiadosgame/ruta1/view/Madrid.java
+++ app/src/main/java/es/riberadeltajo/refugiadosgame/ruta1/view/Madrid.java
@@ -12,6 +12,6 @@ public class Madrid extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madrid);
         //prueba2
-        //pruebachochespss
+        //pruebachochesps
     }
 }
