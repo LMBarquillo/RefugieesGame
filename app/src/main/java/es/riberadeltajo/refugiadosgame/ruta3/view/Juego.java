3b75bbb5781145ad709b284edf2bdb96e4b7deaf
lunaton <lunaton@yahoo.com>
2017-02-13 12:52:28 +0100

    Arcade

--- app/src/main/java/es/riberadeltajo/refugiadosgame/ruta3/view/Juego.java
+++ app/src/main/java/es/riberadeltajo/refugiadosgame/ruta3/view/Juego.java
@@ -18,17 +18,19 @@ public class Juego {
     private GameView gameView;
     private Bitmap terreno;
     private MediaPlayer musica;
-    private float playerWidthScale = .092f; //sobre 100px
-    private float playerHeightScale = .104f; //sobre 200px
-    private float nenufarWidthScale = .133f; // sobre 145px
-    private float nenufarHeightScale = .074f; //sobre 145px
-    private float nenufarPosYScale = .191f; //sobre 362px
-    private float aguaYScale = .192f; //sobre 369
-    private float aguaHeightScale = .593f; //sobre 1140
+    private final float playerWidthScale = .092f; //sobre 100px
+    private final float playerHeightScale = .104f; //sobre 200px
+    private final float playerSpeedYScale = .005f; // sobre 10px;
+    private final float nenufarWidthScale = .133f; // sobre 145px
+    private final float nenufarHeightScale = .074f; //sobre 145px
+    private final float nenufarPosYScale = .191f; //sobre 362px
+    private final float nenufarSpeedXScale = .009f; //sobre 10px
+    private final float aguaYScale = .192f; //sobre 369px
+    private final float aguaHeightScale = .593f; //sobre 1140px
 
-    private Sprite player;
+    private Player player;
     private Bitmap bmpNenufar;
-    private ArrayList<Sprite> nenufares;
+    private ArrayList<Nenufar> nenufares;
 
     public Juego(GameView gameView) {
         setGameView(gameView);
@@ -36,8 +38,8 @@ public class Juego {
         //agua = Bitmap.createScaledBitmap(agua, getGameView().getWidth(), getGameView().getHeight(), false);
         terreno = BitmapFactory.decodeResource(getGameView().getResources(), R.drawable.sarajevocarretera);
         terreno = Bitmap.createScaledBitmap(terreno, getGameView().getWidth(), getGameView().getHeight(), false);
-        player = new Sprite(getGameView(), R.drawable.milan_personajeuno, 4, 4, (int)(getGameView().getWidth() * playerWidthScale), (int)(getGameView().getHeight() * playerHeightScale), (getGameView().getWidth() - 100) / 2, getGameView().getHeight() - 200, 0, 0);
-        nenufares = new ArrayList<Sprite>();
+        player = new Player(getGameView(), R.drawable.milan_personajeuno, 4, 4, (int)(getGameView().getWidth() * playerWidthScale), (int)(getGameView().getHeight() * playerHeightScale), (getGameView().getWidth() - 100) / 2, getGameView().getHeight() - 200, 0, 0);
+        nenufares = new ArrayList<Nenufar>();
         crearNenufares();
     }
 
@@ -49,11 +51,11 @@ public class Juego {
         this.gameView = gameView;
     }
 
-    public Sprite getPlayer() {
+    public Player getPlayer() {
         return player;
     }
 
-    public void setPlayer(Sprite player) {
+    public void setPlayer(Player player) {
         this.player = player;
     }
 
@@ -70,8 +72,10 @@ public class Juego {
         int height = (int)(getGameView().getHeight() * nenufarHeightScale);
         int y = (int)(getGameView().getHeight() * nenufarPosYScale);
         bmpNenufar = BitmapFactory.decodeResource(getGameView().getResources(), R.drawable.sarajevonenufar);
+        int speed;
         for(int i = 1; i < 9; i++) {
-            nenufares.add(new Sprite(getGameView(), bmpNenufar, 1, 1, width, height, (i%2==0?0:getGameView().getWidth() - width), y+(width*(i-1)), (int)(Math.random()*5 + 5), 0));
+            speed = (int)(Math.random()*(getGameView().getWidth() * nenufarSpeedXScale) + (getGameView().getWidth() * nenufarSpeedXScale));
+            nenufares.add(new Nenufar(getGameView(), bmpNenufar, width, height, (i%2==0?-width:getGameView().getWidth()), y+(width*(i-1)), speed, 0));
         }
 
     }
@@ -90,8 +94,13 @@ public class Juego {
         } else {
             player.setSafe(true);
         }
-        for(Sprite s : nenufares) {
-            if(s.getPosX() < 0 - s.getWidth() || s.getPosX() > getGameView().getWidth()) {
+        for(Nenufar s : nenufares) {
+            if(s.getPosX() + s.getSpeedX() < 0) {
+                s.setPosX(0);
+                s.setSpeedX(-s.getSpeedX());
+            } else
+            if(s.getPosX() + s.getWidth() + s.getSpeedX() > getGameView().getWidth()) {
+                s.setPosX(getGameView().getWidth() - s.getWidth());
                 s.setSpeedX(-s.getSpeedX());
             }
             if(s.isCollition(player)) {
@@ -101,6 +110,18 @@ public class Juego {
             s.draw(canvas);
         }
         player.draw(canvas);
+    }
+
+    public void andarUp() {
+        getPlayer().setSpeedY((int)(-getGameView().getHeight() * playerSpeedYScale));
+    }
+
+    public void andarDown() {
+        getPlayer().setSpeedY((int)(getGameView().getHeight() * playerSpeedYScale));
+    }
+
+    public void andarStop() {
+        getPlayer().setSpeedY(0);
     }
 
     public void touch(int x, int y) {
