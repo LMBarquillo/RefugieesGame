3185ab523e19fae220b66c238e583b963bb53cd0
AdRiiPO95 <adrilink95@gmail.com>
2017-02-15 18:15:22 +0100

    Añadido dialogo al finalizar

--- app/src/main/java/es/riberadeltajo/refugiadosgame/ruta1/view/Madrid.java
+++ app/src/main/java/es/riberadeltajo/refugiadosgame/ruta1/view/Madrid.java
@@ -1,16 +1,19 @@
 package es.riberadeltajo.refugiadosgame.ruta1.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
-import es.riberadeltajo.refugiadosgame.R;
+import es.riberadeltajo.refugiadosgame.ruta2.view.Milan;
 
 public class Madrid extends AppCompatActivity {
     private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -22,5 +25,9 @@ public class Madrid extends AppCompatActivity {
         gameView=new GameView(this);
         setContentView(gameView);
 
+    }
+
+    public void continuar() {
+        startActivity(new Intent(this, Milan.class));
     }
 }
