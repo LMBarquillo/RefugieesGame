48b6531885a5911b7206e9b18460fcb87db23fff
lunaton <lunaton@yahoo.com>
2017-02-24 11:02:56 +0100

    Menu principal

---
+++ app/src/main/java/es/riberadeltajo/refugiadosgame/ruta3/view/ruta/SarajevoChino.java
@@ -0,0 +1,12 @@
+package es.riberadeltajo.refugiadosgame.ruta3.view.ruta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SarajevoChino extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
