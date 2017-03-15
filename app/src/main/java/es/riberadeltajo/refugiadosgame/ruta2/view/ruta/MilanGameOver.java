455d2ca4fb014ee5bb9a2a21c092664615b99196
lunaton <lunaton@yahoo.com>
2017-03-15 02:31:52 +0100

    Progreso del jugador completado y adaptado para todas las rutas

--- app/src/main/java/es/riberadeltajo/refugiadosgame/ruta2/view/ruta/MilanGameOver.java
+++ app/src/main/java/es/riberadeltajo/refugiadosgame/ruta2/view/ruta/MilanGameOver.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import es.riberadeltajo.refugiadosgame.R;
 
@@ -13,6 +14,18 @@ public class MilanGameOver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_milan_game_over);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); //Evita que se apague la pantalla
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     public void intentar(View view){
